a2cb7759cbe532246a66be84720d0edd883e4294
flowsar <brahim.sar151@gmail.com>
2025-09-09 09:56:35 +0100

    generate requirement fiel

--- blog-post/db.py
+++ blog-post/db.py
@@ -6,7 +6,7 @@ from psycopg2.extras import RealDictCursor
 # replace all these data with your own database data
 connect = psycopg2.connect(
     user="sardb",
-    password="password", 
+    password="Root890", 
     host="localhost",
     port="5433", # default port of postgres is 5432
     database="blog_post",
